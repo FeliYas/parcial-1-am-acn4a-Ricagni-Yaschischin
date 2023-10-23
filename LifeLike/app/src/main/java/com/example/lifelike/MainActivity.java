4cd210d705d20586de44040ac2e432e3017e8f60
tutte7 <86837761+tutte7@users.noreply.github.com>
2023-10-23 00:56:13 -0300

    Add files via upload

---
+++ LifeLike/app/src/main/java/com/example/lifelike/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.lifelike;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Código primer botón
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View button) {
+                LinearLayout content = findViewById(R.id.content);
+                TextView newText = new TextView(button.getContext());
+                newText.setText(R.string.newtext);
+                content.addView(newText);
+            }
+        });
+
+        // Código segundo botón
+        Button btnClickMe = findViewById(R.id.button4);
+        TextView textResult = findViewById(R.id.textResult);
+        btnClickMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textResult.setText("¡Redirigiendote para que te puedas Registrar!");
+            }
+        });
+    }
+
+
+
+}
